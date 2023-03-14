794f8d20a67f40f24448f876fed82b4659c1c4c3
Shubh <workoho001@gmail.com>
2023-03-14 16:37:40 +0530

    committ

--- revise.c
+++ revise.c
@@ -50,14 +50,35 @@ int main()
 
 //5 area of triangle
     
-    float area, a, b, c, s;
+    // float area, a, b, c, s;
 
-    printf("Enter the values of sides of triangle: \n");
-    scanf("%f %f %f", &a,&b, &c);
+    // printf("Enter the values of sides of triangle: \n");
+    // scanf("%f %f %f", &a,&b, &c);
 
-    s= (a+b+c)/2;
-    area = sqrt(s*(s-a)*(s-b)*(s-c));
-    printf("area of triangle is: %0.2f\n", area);
+    // s= (a+b+c)/2;
+    // area = sqrt(s*(s-a)*(s-b)*(s-c));
+    // printf("area of triangle is: %0.2f\n", area);
 
-    return 0;
+//6 reversing with basic knowledge
+    int n, d1, d2, d3, d4, d5,m;
+    long int rev;
+
+    printf("Enter your five digit number\n");
+    scanf("%d", &n);
+
+    d5 = n%10;
+    n = n/10;
+    d4 = n%10;
+    n = n/10;
+    d3 = n%10;
+    n = n/10;
+    d2 = n%10;
+    n = n/10;
+    d1 = n%10;
+    
+
+    rev = d5*10000 + d4*1000 + d3*100 + d2*10 + d1;
+    printf("\nthe reverse number is: %ld", rev);
+    
+     return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,31 +1,17 @@
-int b = 50;
-        int func(int a1)
-        {
-                // int un = 22;
-                // printf("value of b is: %d\n", b);
+// printf("Enter your five digit number\n");
+    // scanf("%d", &n);
 
-                static int st = 0;
-                printf("the value of st is %d\n", st);
-                st++;
-                return a1+st;
-        }
-        int main(int argc, char const *argv[])
-        {
-                int a = 10, un = 22;
-                int val = func(a);
-                
-                val = func(a);
+    // d5 = n%10;
+    // n = n/10;
+    // d4 = n%10;
+    // n = n/10;
+    // d3 = n%10;
+    // n = n/10;
+    // d2 = n%10;
+    // n = n/10;
+    // d1 = n%10;
+    
 
-                val = func(a);
-                int *pts = &val;
-
-        //printf("%d\n", un);
-
-        //printf("value of a is %d\n", a);
-                // printf("value of function is %d\n", val);
-        //printf("value of function is %d\n", val);
-                // printf("address of 'a' is: %d\n", &a);
-                // printf("address of 'func()' is: %d", &val);
-                
-        return 0;
-        }
+    // rev = d5*10000 + d4*1000 + d3*100 + d2*10 + d1;
+    // printf("\nthe reverse number is: %ld", rev);
+    
